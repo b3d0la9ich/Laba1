406f919419d8dbcba9e87d79145514bc577f5f90
bedolaga <tnikita912@gmail.com>
2024-03-17 23:03:47 +0700

    rectangle

---
+++ file.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+    int a = 5, b = 3;
+    cout << 2 * (a + b) << endl;
+    cout << (a * b) << endl;
+    cout << sqrt(a*a + b*b) << endl;
+    return 0;
+}
