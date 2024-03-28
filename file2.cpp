0182b0652b75453aa2b6cd4241a0a7f6c221efd5
Nikerersd <nikrudk0@yandex.ru>
2024-03-29 01:54:36 +0700

    Area

--- file2.cpp
+++ file2.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int a, b, c;
+    double a, b, c;
     cout << "Write your sides:" << endl;
     cin >> a >> b >> c;
     cout << "Calculating the perimeter, the area according to the Heron formula, checking for isosceles" << endl;
-    int per = a + b + c;
+    double per = a + b + c;
     cout << per << endl;
-    int polyper = per / 2;
-    cout << sqrt(polyper*(polyper - a)*(polyper - b) * (polyper - c)) << endl;
+    double polyper = per / 2.0;
+    double area = sqrt(polyper * (polyper - a) * (polyper - b) * (polyper - c));
+    cout << area << endl;
     if (a == b || a == c || b == c)
     {
         cout << "The triangle is isosceles" << endl;
