5f641423dd677e393f2268a707bf7100b133b496
Nikerersd <nikrudk0@yandex.ru>
2024-03-29 11:47:24 +0700

    priam

--- file.cpp
+++ file.cpp
@@ -8,9 +8,14 @@ int main()
     int a, b;
     cout << "Write your sides:" << endl;
     cin >> a >> b;
-    cout << "Calculate the perimeter, area, diagonal length..." << endl;
-    cout << 2 * (a + b) << endl;
-    cout << (a * b) << endl;
-    cout << sqrt(a*a + b*b) << endl;
+    if (a <= 0 || b <= 0){
+        cout << "wrong sides!" << endl;
+    }
+    else{
+        cout << "Calculate the perimeter, area, diagonal length..." << endl;
+        cout << 2 * (a + b) << endl;
+        cout << (a * b) << endl;
+        cout << sqrt(a*a + b*b) << endl;
+    }
     return 0;
 }
